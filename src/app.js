0aef80f1ba4a36f19ddf392a6c22fdfa91082694
Alexey Kotlyarov <a@koterpillar.com>
2015-10-04 19:43:17 +1100

    Try to build

--- src/app.js
+++ src/app.js
@@ -2,31 +2,24 @@
  * Main application code.
  */
 
+// TODO: Support location setting
+var LOCATION = "Melbourne";
+
 var ajax = require('ajax');
 var UI = require('ui');
+
+Pebble.timelineSubscriptions(
+  function(topics) {
+    if (topics.length === 0) {
+      Pebble.timelineSubscribe(LOCATION);
+    }
+  }
+);
 
 var main = new UI.Card({
   title: 'UV Alert',
   subtitle: 'Useless screen',
   body: 'Press any button.'
 });
-
-function sendToken() {
-  ajax(
-      {
-        url: SERVER_URL,
-        method: 'POST',
-        data: {
-          'token': getToken(),
-        }
-      },
-      function (data) {
-        main.subtitle("Token sent.");
-      },
-      function (error) {
-        main.subtitle("Error sending token.");
-      }
-  );
-}
 
 main.show();
