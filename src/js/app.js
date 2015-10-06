e6cd4cffc2ac5be407f57948b64b38ad2494f6e1
Alexey Kotlyarov <a@koterpillar.com>
2015-10-07 08:22:51 +1100

    Refactor JS

--- src/js/app.js
+++ src/js/app.js
@@ -2,25 +2,41 @@
  * Main application code.
  */
 
-Pebble.addEventListener('ready',
-  function (e) {
-    console.log("ready from rrrrrr");
-  }
-);
-
 // TODO: Support location setting
 var LOCATION = "Melbourne";
 
+function pass() {}
+
+function logError(where) {
+  return function (errorString) {
+    console.log("Error: " + where + ": " + errorString);
+  };
+}
+
+function setLocationSubscription(loc) {
+  Pebble.timelineSubscriptions(
+    function (topics) {
+      var haveSubscription = false;
+
+      for (var i = 0; i < topics.length; i++) {
+        if (topics[i] == loc) {
+          haveSubscription = true;
+        } else {
+          Pebble.timelineUnsubscribe(
+              topics[i], pass, logError('timelineUnsubscribe'));
+        }
+
+        if (!haveSubscription) {
+          Pebble.timelineSubscribe(loc, pass, logError('timelineSubscribe'));
+        }
+      }
+    },
+    logError('timelineSubscriptions')
+  );
+}
+
 var ajax = require('ajax');
 var UI = require('ui');
-
-Pebble.timelineSubscriptions(
-  function(topics) {
-    if (topics.length === 0) {
-      Pebble.timelineSubscribe(LOCATION);
-    }
-  }
-);
 
 var main = new UI.Card({
   title: 'UV Alert',
@@ -29,3 +45,5 @@ var main = new UI.Card({
 });
 
 main.show();
+
+setLocationSubscription(LOCATION);
